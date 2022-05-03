7948563b95f1437fe082ce70d653fbd29cd9806a
kkazum <49956319+kkazum@users.noreply.github.com>
2022-05-03 16:53:33 +0900

    設定変更

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -7,13 +7,11 @@ import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
 import isDev from "electron-is-dev";
 import prepareNext from "electron-next";
 
-// Prepare the renderer once the app is ready
 app.on("ready", async () => {
   await prepareNext("./renderer");
-
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: false,
